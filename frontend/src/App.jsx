ccfd410235cbba77c69dc41cefef1a340462770b
chyellow <christopher.hellriegel0122@gmail.com>
2026-01-07 15:58:59 -0500

    Choose avatar element

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-
+import Button from './Button.jsx'
+import ProfilePicture from './ProfilePicture.jsx';
+import MyComponent from './MyComponent.jsx';
+import Counter from './Counter.jsx';
+import ColorPicker from './ColorPicker.jsx';
+import MyComponent2 from './MyComponent2.jsx';
+import MyComponent3 from './MyComponent3.jsx';
+import MyComponent4 from './MyComponent4.jsx';
+import ToDoList from './ToDoList.jsx';
+import MyComponent5 from './MyComponent5.jsx';
+import ChooseAvatar from './ChooseAvatar.jsx';
 
 function App() {
 
   return(
-   <h1>Practice</h1>
+    <><ChooseAvatar/></>
   )
+  
 }
 
 export default App;
---
+++ frontend/src/ChooseAvatar.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import styles from "./ChooseAvatar.module.css";
+import img1 from './assets/Billy.jpg'
+import img2 from './assets/Brandon.jpg'
+import img3 from './assets/Brandon2.jpg'
+import img4 from './assets/Chris.jpg'
+import img5 from './assets/Jason.jpg'
+import img6 from './assets/Jason2.jpg'
+import img7 from './assets/Louis.jpg'
+import img8 from './assets/Louis2.jpg'
+import img9 from './assets/Nick.jpg'
+import img10 from './assets/Nick2.jpg'
+import img11 from './assets/Ryan.jpg'
+import img12 from './assets/Ryan2.jpg'
+import img13 from './assets/trev.jpg'
+import img14 from './assets/Yankees.jpg'
+import leftArrow from './assets/arrow-left.png'
+import rightArrow from './assets/arrow-right.png'
+
+
+
+function ChooseAvatar(){
+
+    const [index, setIndex] = useState(0);
+    const avatars = [img1, img2, img3, img4, img5, img6, 
+          img7, img8, img9, img10, img11, img12, img13, img14];
+
+    function selectLeft(){
+        setIndex((prev) => prev === 0 ? avatars.length - 1 : prev - 1)
+    }
+
+    function selectRight(){
+        setIndex(prev => prev === avatars.length - 1 ? 0 : prev + 1)
+    }
+
+    return (
+    <div className={styles.avatarBox}>
+        <button className={styles.arrow} onClick={selectLeft}>
+        <img src={leftArrow} alt="Left" />
+        </button>
+
+        <img className={styles.avatar} src={avatars[index]} alt="Avatar" />
+
+        <button className={styles.arrow} onClick={selectRight}>
+        <img src={rightArrow} alt="Right" />
+        </button>
+    </div>
+    );
+
+}
+
+export default ChooseAvatar;
